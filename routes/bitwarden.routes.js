f1a609f08d57db20b73c7a8756fd6c8388ac3aeb
mistWil <7448@holbertonstudents.com>
2024-06-07 15:52:29 +0200

    new archi

--- routes/bitwarden.routes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const router = express.Router();
-const bitwardenController = require("../controllers/bitwarden.controller");
-
-router.get("/download", bitwardenController.getDownloadLink);
-
-module.exports = router;
